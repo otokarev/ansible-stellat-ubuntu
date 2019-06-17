8389a9ca3d0fa9c562b845f55256b179d536ef10
Oleg Tokarev <otokarev@openprovider.ru>
2019-06-17 17:46:09 +0700

    [*]

--- reingest.sh
+++ reingest.sh
@@ -25,3 +25,17 @@ nohup stellar-horizon-cmd db reingest range 16000000 18000000 > reingest-1800000
 nohup stellar-horizon-cmd db reingest range 18000000 20000000 > reingest-20000000.log &
 nohup stellar-horizon-cmd db reingest range 20000000 22000000 > reingest-22000000.log &
 nohup stellar-horizon-cmd db reingest range 22000000 24130000 > reingest-24130000.log &
+
+nohup stellar-horizon-cmd db reingest range 18000000 18500000 > reingest-18500000.log &
+nohup stellar-horizon-cmd db reingest range 18500000 19000000 > reingest-19000000.log &
+nohup stellar-horizon-cmd db reingest range 19000000 19500000 > reingest-19500000.log &
+nohup stellar-horizon-cmd db reingest range 19500000 20000000 > reingest-20000000.log &
+nohup stellar-horizon-cmd db reingest range 20000000 20500000 > reingest-20500000.log &
+nohup stellar-horizon-cmd db reingest range 20500000 21000000 > reingest-21000000.log &
+nohup stellar-horizon-cmd db reingest range 21000000 21500000 > reingest-21500000.log &
+nohup stellar-horizon-cmd db reingest range 21500000 22000000 > reingest-22000000.log &
+nohup stellar-horizon-cmd db reingest range 22000000 22500000 > reingest-22500000.log &
+nohup stellar-horizon-cmd db reingest range 22500000 23000000 > reingest-23000000.log &
+nohup stellar-horizon-cmd db reingest range 23000000 23500000 > reingest-23500000.log &
+nohup stellar-horizon-cmd db reingest range 23500000 24000000 > reingest-24000000.log &
+nohup stellar-horizon-cmd db reingest range 24000000 24500000 > reingest-24500000.log &
